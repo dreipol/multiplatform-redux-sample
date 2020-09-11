4baae61eb6663477daf7e5d7a1d9460932db4f64
Kai Widmer <>
2020-09-11 10:12:45 +0200

    [data_model] Add disposal

---
+++ shared/src/commonMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/database/DisposalType.kt
@@ -0,0 +1,12 @@
+package ch.dreipol.multiplatform.reduxsample.shared.database
+
+enum class DisposalType {
+    CARTON,
+    BIO_WASTE,
+    PAPER,
+    E_TRAM,
+    CARGO_TRAM,
+    TEXTILES,
+    HAZARDOUS_WASTE,
+    SWEEPINGS,
+}
