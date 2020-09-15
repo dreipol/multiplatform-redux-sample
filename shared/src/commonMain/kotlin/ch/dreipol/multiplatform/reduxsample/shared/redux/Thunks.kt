26469ef3c703c469673b5f3c6d7d5ba6de854aed
Kai Widmer <>
2020-09-15 11:26:15 +0200

    [android_dashboard] Fix syncThunk

--- shared/src/commonMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/Thunks.kt
+++ shared/src/commonMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/Thunks.kt
@@ -1,14 +1,20 @@
 package ch.dreipol.multiplatform.reduxsample.shared.redux
 
-import ch.dreipol.dreimultiplatform.launchAndWait
+import ch.dreipol.dreimultiplatform.defaultDispatcher
 import ch.dreipol.multiplatform.reduxsample.shared.database.DisposalDataStore
 import ch.dreipol.multiplatform.reduxsample.shared.database.DisposalType
 import ch.dreipol.multiplatform.reduxsample.shared.network.ServiceFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import org.reduxkotlin.Thunk
 
+val networkScope = CoroutineScope(defaultDispatcher)
+
 fun syncDisposalsThunk(): Thunk<AppState> = { dispatch, _, _ ->
-    launchAndWait { ServiceFactory.disposalService().syncDisposals(DisposalType.CARTON) }
-    dispatch(loadDisposalsThunk())
+    networkScope.launch {
+        ServiceFactory.disposalService().syncDisposals(DisposalType.CARTON)
+        dispatch(loadDisposalsThunk())
+    }
 }
 
 fun loadDisposalsThunk(): Thunk<AppState> = { dispatch, _, _ ->
