d73a2b557cb53d092c4d3f64c3654f38785946be
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2020-09-17 19:46:16 +0200

    [setup_redux] Cleanup

--- shared/src/commonMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/navigation/NavigationReducer.kt
+++ shared/src/commonMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/navigation/NavigationReducer.kt
@@ -2,7 +2,6 @@ package ch.dreipol.multiplatform.reduxsample.shared.redux.navigation
 
 import ch.dreipol.dreimultiplatform.reduxkotlin.navigation.NavigationState
 import ch.dreipol.dreimultiplatform.reduxkotlin.navigation.navigateBack
-import ch.dreipol.multiplatform.reduxsample.shared.redux.NavigationAction
 import org.reduxkotlin.Reducer
 
 val navigationReducer: Reducer<NavigationState> = { state, action ->
