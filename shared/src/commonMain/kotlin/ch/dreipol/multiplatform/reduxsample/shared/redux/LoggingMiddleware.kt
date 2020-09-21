047261b852e794bf33486c5cb453b94bc7fd4283
Julia Strasser <julia.strasser@dreipol.ch>
2020-09-21 17:30:05 +0200

    [android_dashboard] Improve logging

--- shared/src/commonMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/LoggingMiddleware.kt
+++ shared/src/commonMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/LoggingMiddleware.kt
@@ -4,9 +4,14 @@ import ch.dreipol.dreimultiplatform.kermit
 import org.reduxkotlin.middleware
 
 val loggerMiddleware = middleware<AppState> { _, next, action ->
+    val actionLogName =
+        when (action) {
+            is Function<*> -> "Thunk Function"
+            else -> action::class.simpleName
+        }
     kermit().d {
         "\n********************************************\n" +
-            "* DISPATCHED action: $action\n" +
+            "******** DISPATCHED action: $actionLogName\n" +
             "********************************************"
     }
     next(action)
