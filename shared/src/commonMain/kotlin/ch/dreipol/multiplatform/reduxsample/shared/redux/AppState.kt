50116a5431dbf5ce80d289118ff42fa8a2ed3c40
Kai Widmer <>
2020-09-22 15:45:48 +0200

    [onboarding] Add OnboardingView

--- shared/src/commonMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/AppState.kt
+++ shared/src/commonMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/AppState.kt
@@ -4,12 +4,16 @@ import ch.dreipol.dreimultiplatform.reduxkotlin.navigation.NavigationDirection
 import ch.dreipol.dreimultiplatform.reduxkotlin.navigation.NavigationState
 import ch.dreipol.multiplatform.reduxsample.shared.redux.navigation.MainScreen
 import ch.dreipol.multiplatform.reduxsample.shared.ui.DashboardViewState
+import ch.dreipol.multiplatform.reduxsample.shared.ui.OnboardingViewState
+import ch.dreipol.multiplatform.reduxsample.shared.ui.OnboardingZipStep
 
 private val initialNavigationState = NavigationState(listOf(MainScreen.DASHBOARD), NavigationDirection.PUSH)
+private val initialOnboardingState = OnboardingZipStep(1, "", "", emptyList())
 
 data class AppState(
     val navigationState: NavigationState = initialNavigationState,
-    val dashboardViewState: DashboardViewState = DashboardViewState()
+    val dashboardViewState: DashboardViewState = DashboardViewState(),
+    val onboardingViewState: OnboardingViewState = initialOnboardingState
 ) {
 
     companion object {
---
+++ shared/src/commonMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/ui/OnboardingView.kt
@@ -0,0 +1,30 @@
+package ch.dreipol.multiplatform.reduxsample.shared.ui
+
+interface OnboardingViewState {
+    val step: Int
+    val title: String
+    val primary: String
+}
+
+data class OnboardingZipStep(
+    override val step: Int,
+    override val title: String,
+    override val primary: String,
+    val zips: List<String>) :  OnboardingViewState
+
+data class GenericOnboardingStep(
+    override val step: Int,
+    override val title: String,
+    override val primary: String) : OnboardingViewState
+
+
+interface OnboardingView : BaseView {
+    fun render(viewState: OnboardingViewState)
+    override fun presenter() = onboardingPresenter
+}
+
+val onboardingPresenter = presenter<OnboardingView> {
+    {
+        select({ it.onboardingViewState }) { render(state.onboardingViewState) }
+    }
+}
