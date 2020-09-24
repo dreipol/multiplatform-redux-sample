f15ae384d4b3910cb9039b24617756ae71685feb
Kai Widmer <>
2020-09-24 11:46:02 +0200

    [cleanup_middleware] Update tests

--- shared/src/commonTest/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/NavigationReducerTest.kt
+++ shared/src/commonTest/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/NavigationReducerTest.kt
@@ -1,7 +1,7 @@
 package ch.dreipol.multiplatform.reduxsample.shared.redux
 
+import ch.dreipol.multiplatform.reduxsample.shared.redux.navigation.MainScreen
 import ch.dreipol.multiplatform.reduxsample.shared.redux.navigation.NavigationAction
-import ch.dreipol.multiplatform.reduxsample.shared.redux.navigation.OnboardingNavigation
 import ch.dreipol.multiplatform.reduxsample.shared.redux.navigation.OnboardingScreen
 import ch.dreipol.multiplatform.reduxsample.shared.redux.navigation.navigationReducer
 import kotlin.test.Test
@@ -12,12 +12,12 @@ class NavigationReducerTest {
     @Test
     fun testOnboardingNavigation() {
         var navigationState = AppState.INITIAL_STATE.navigationState
-        navigationState = navigationReducer(navigationState, OnboardingNavigation())
+        navigationState = navigationReducer(navigationState, NavigationAction.ONBOARDING_START)
         assertEquals(1, navigationState.screens.size)
         var lastScreen = navigationState.screens.last() as OnboardingScreen
         assertEquals(1, lastScreen.step)
 
-        navigationState = navigationReducer(navigationState, OnboardingNavigation(2))
+        navigationState = navigationReducer(navigationState, NavigationAction.ONBOARDING_NEXT)
         assertEquals(2, navigationState.screens.size)
         lastScreen = navigationState.screens.last() as OnboardingScreen
         assertEquals(2, lastScreen.step)
@@ -26,5 +26,9 @@ class NavigationReducerTest {
         assertEquals(1, navigationState.screens.size)
         lastScreen = navigationState.screens.last() as OnboardingScreen
         assertEquals(1, lastScreen.step)
+
+        navigationState = navigationReducer(navigationState, NavigationAction.ONBOARDING_END)
+        assertEquals(1, navigationState.screens.size)
+        assertEquals(MainScreen.DASHBOARD, navigationState.screens.last())
     }
 }
--- shared/src/commonTest/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/OnboardingViewReducerTest.kt
+++ shared/src/commonTest/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/OnboardingViewReducerTest.kt
@@ -4,11 +4,6 @@ import ch.dreipol.multiplatform.reduxsample.shared.database.SettingsDataStore
 import ch.dreipol.multiplatform.reduxsample.shared.delight.Settings
 import ch.dreipol.multiplatform.reduxsample.shared.redux.actions.SettingsLoadedAction
 import ch.dreipol.multiplatform.reduxsample.shared.redux.actions.ZipUpdatedAction
-import ch.dreipol.multiplatform.reduxsample.shared.redux.navigation.NavigationAction
-import ch.dreipol.multiplatform.reduxsample.shared.redux.navigation.OnboardingNavigation
-import ch.dreipol.multiplatform.reduxsample.shared.ui.EnterZipState
-import ch.dreipol.multiplatform.reduxsample.shared.ui.OnboardingViewState
-import ch.dreipol.multiplatform.reduxsample.shared.ui.SelectDisposalTypesState
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
@@ -16,22 +11,8 @@ import kotlin.test.assertNull
 class OnboardingViewReducerTest {
 
     @Test
-    fun testOnboardingNavigation() {
+    fun loadSettingsTest() {
         var onboardingViewState = AppState.INITIAL_STATE.onboardingViewState
-        onboardingViewState = onboardingViewReducer(onboardingViewState, OnboardingNavigation())
-        assertEquals(1, onboardingViewState.step)
-
-        onboardingViewState = onboardingViewReducer(onboardingViewState, OnboardingNavigation(2))
-        assertEquals(2, onboardingViewState.step)
-
-        onboardingViewState = onboardingViewReducer(onboardingViewState, NavigationAction.BACK)
-        assertEquals(1, onboardingViewState.step)
-    }
-
-    @Test
-    fun testOnboardingStart() {
-        var onboardingViewState = AppState.INITIAL_STATE.onboardingViewState
-        onboardingViewState = onboardingViewReducer(onboardingViewState, OnboardingNavigation())
         assertNull(onboardingViewState.enterZipState.selectedZip)
 
         onboardingViewState =
@@ -48,10 +29,9 @@ class OnboardingViewReducerTest {
 
     @Test
     fun enterZipTest() {
-        var onboardingViewState = OnboardingViewState(
-            1, "", "", EnterZipState(emptyList(), 8000),
-            SelectDisposalTypesState(true, true, true, true, true, true, true, true)
-        )
+        var onboardingViewState = AppState.INITIAL_STATE.onboardingViewState
+        assertNull(onboardingViewState.enterZipState.selectedZip)
+
         onboardingViewState = onboardingViewReducer(onboardingViewState, ZipUpdatedAction(9000))
         assertEquals(9000, onboardingViewState.enterZipState.selectedZip)
 
