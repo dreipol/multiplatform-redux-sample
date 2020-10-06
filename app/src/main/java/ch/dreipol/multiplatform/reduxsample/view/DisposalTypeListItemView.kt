4f1cad3e883f96d32221e34cf8a8f9594257e90d
Kai Widmer <>
2020-10-06 11:49:49 +0200

    [android_onboarding_styled] Add disposalTypeListItemView

---
+++ app/src/main/java/ch/dreipol/multiplatform/reduxsample/view/DisposalTypeListItemView.kt
@@ -0,0 +1,25 @@
+package ch.dreipol.multiplatform.reduxsample.view
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import ch.dreipol.multiplatform.reduxsample.databinding.ViewDisposalTypeListItemBinding
+
+class DisposalTypeListItemView(context: Context) : LinearLayout(context) {
+
+    private val binding = ViewDisposalTypeListItemBinding.inflate(LayoutInflater.from(context))
+
+    fun init(icon: Int?, text: String, selected: Boolean) {
+        if (icon == null) {
+            binding.icon.visibility = View.GONE
+        } else {
+            binding.icon.visibility = View.VISIBLE
+            binding.icon.setImageResource(icon)
+        }
+        binding.text.text = text
+        binding.toggle.isActivated = selected
+
+    }
+
+}
