b289661ed74b3082d45ddb1ba8aa95ebe3722207
Kai Widmer <>
2020-10-08 15:05:47 +0200

    [android_onboarding_styled] Refactor

--- app/src/main/java/ch/dreipol/multiplatform/reduxsample/fragments/onboarding/SelectDisposalTypesFragment.kt
+++ app/src/main/java/ch/dreipol/multiplatform/reduxsample/fragments/onboarding/SelectDisposalTypesFragment.kt
@@ -1,20 +1,14 @@
 package ch.dreipol.multiplatform.reduxsample.fragments.onboarding
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import ch.dreipol.dreimultiplatform.reduxkotlin.rootDispatch
 import ch.dreipol.multiplatform.reduxsample.databinding.FragmentOnboardingSelectDisposalTypesBinding
-import ch.dreipol.multiplatform.reduxsample.databinding.ViewDisposalTypeListItemBinding
 import ch.dreipol.multiplatform.reduxsample.shared.database.DisposalType
-import ch.dreipol.multiplatform.reduxsample.shared.redux.actions.UpdateShowDisposalType
 import ch.dreipol.multiplatform.reduxsample.shared.ui.BaseOnboardingSubState
 import ch.dreipol.multiplatform.reduxsample.shared.ui.SelectDisposalTypesState
-import ch.dreipol.multiplatform.reduxsample.utils.getDrawableIdentifier
-import ch.dreipol.multiplatform.reduxsample.utils.getString
+import ch.dreipol.multiplatform.reduxsample.view.SelectDisposalTypesAdapter
 
 class SelectDisposalTypesFragment : OnboardingFragment() {
 
@@ -44,31 +38,5 @@ class SelectDisposalTypesFragment : OnboardingFragment() {
         data[DisposalType.SWEEPINGS] = onboardingSubState.showSweepings
         selectDisposalTypesAdapter.disposalTypes = data
         selectDisposalTypesAdapter.notifyDataSetChanged()
-    }
-}
-
-class SelectDisposalTypesViewHolder(val disposalTypeListItemBinding: ViewDisposalTypeListItemBinding) :
-    RecyclerView.ViewHolder(disposalTypeListItemBinding.root)
-
-class SelectDisposalTypesAdapter(private val context: Context, var disposalTypes: Map<DisposalType, Boolean>) :
-    RecyclerView.Adapter<SelectDisposalTypesViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SelectDisposalTypesViewHolder {
-        return SelectDisposalTypesViewHolder(ViewDisposalTypeListItemBinding.inflate(LayoutInflater.from(context), parent, false))
-    }
-
-    override fun onBindViewHolder(holder: SelectDisposalTypesViewHolder, position: Int) {
-        val item = disposalTypes.entries.toList()[position]
-        holder.disposalTypeListItemBinding.text.text = context.getString(item.key.translationKey)
-        holder.disposalTypeListItemBinding.icon.setImageResource(context.getDrawableIdentifier(item.key.iconId))
-        holder.disposalTypeListItemBinding.toggle.setOnCheckedChangeListener { _, _ -> }
-        holder.disposalTypeListItemBinding.toggle.isChecked = item.value
-        holder.disposalTypeListItemBinding.toggle.setOnCheckedChangeListener { _, isChecked ->
-            rootDispatch(UpdateShowDisposalType(item.key, isChecked))
-        }
-    }
-
-    override fun getItemCount(): Int {
-        return disposalTypes.size
     }
 }
