ceb1a881ded8160b87d2f48d75ab502903b4b17b
Julia Strasser <julia.strasser@dreipol.ch>
2020-10-26 17:26:22 +0100

    [ios] Add colors

---
+++ iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/UIColor+Brand.swift
@@ -0,0 +1,56 @@
+//
+//  UIColor+Brand.swift
+//  Multiplatform Redux Sample
+//
+//  Created by Julia Strasser on 26.10.20.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var testAppBlue: UIColor {
+    return UIColor(red: 0.0, green: 112.0 / 255.0, blue: 188.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var testAppBlueMedium: UIColor {
+    return UIColor(red: 52.0 / 255.0, green: 160.0 / 255.0, blue: 233.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var testAppBlueDark: UIColor {
+    return UIColor(red: 3.0 / 255.0, green: 95.0 / 255.0, blue: 158.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var tetstAppGruen: UIColor {
+    return UIColor(red: 130.0 / 255.0, green: 206.0 / 255.0, blue: 151.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var testAppGreenDark: UIColor {
+    return UIColor(red: 103.0 / 255.0, green: 183.0 / 255.0, blue: 125.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var testAppGreenLight: UIColor {
+    return UIColor(red: 239.0 / 255.0, green: 1.0, blue: 226.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var testAppBlack: UIColor {
+    return UIColor(white: 84.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var testAppTurkis: UIColor {
+    return UIColor(red: 12.0 / 255.0, green: 225.0 / 255.0, blue: 161.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var testAppWhite: UIColor {
+    return UIColor(white: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var testAppLachs: UIColor {
+    return UIColor(red: 1.0, green: 138.0 / 255.0, blue: 138.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var testAppBlueLight: UIColor {
+    return UIColor(red: 221.0 / 255.0, green: 241.0 / 255.0, blue: 1.0, alpha: 1.0)
+  }
+
+}
