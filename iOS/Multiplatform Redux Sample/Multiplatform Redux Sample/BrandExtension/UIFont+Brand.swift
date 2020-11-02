6aba37e54d58cccd2b3a8b045abb474f9b43655b
Julia Strasser <julia.strasser@dreipol.ch>
2020-11-02 14:24:32 +0100

    [ios] Add OpenSans font

--- iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/BrandExtension/UIFont+Brand.swift
+++ iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/BrandExtension/UIFont+Brand.swift
@@ -9,25 +9,41 @@ import Foundation
 import UIKit.UILabel
 
 extension UIFont {
-    //TODO add other fonts
-//    static func h1() -> UIFont {
-//        return UIFont(name: "OpenSans-Regular", size: 28.0)!
-//    }
-//
-//    static func copy1Middle() -> UIFont {
-//        return UIFont.systemFont(ofSize: 15.0, weight: .regular)
-//    }
-//
-//    static func textLink() -> UIFont {
-//        return systemFont(ofSize: 15.0, weight: .bold)
-//    }
 
-    static func button() -> UIFont {
-        //TODO replace with OpenSans Font
-        return systemFont(ofSize: 16.0, weight: .bold)
+    static func h1() -> UIFont {
+        return UIFont(name: "OpenSans-Regular", size: 28.0)!
     }
 
-//    static func lead() -> UIFont {
-//        return systemFont(ofSize: 17.0, weight: .regular)
-//    }
+    static func h2() -> UIFont {
+        return UIFont(name: "OpenSans-Regular", size: 22.0)!
+    }
+
+    static func h3() -> UIFont {
+        return UIFont(name: "OpenSans-Regular", size: 18.0)!
+    }
+
+    static func h4() -> UIFont {
+        return UIFont(name: "OpenSans-SemiBold", size: 16.0)!
+    }
+
+    static func h5() -> UIFont {
+        return UIFont(name: "OpenSans-SemiBold", size: 14.0)!
+    }
+
+    static func label() -> UIFont {
+        return UIFont(name: "OpenSans-Bold", size: 14.0)!
+    }
+
+    static func paragraph1() -> UIFont {
+        return UIFont(name: "OpenSans-Regular", size: 16.0)!
+    }
+
+    static func paragraph2() -> UIFont {
+        return UIFont(name: "OpenSans-Regular", size: 14.0)!
+    }
+
+    static func button() -> UIFont {
+        return UIFont(name: "OpenSans-Bold", size: 16.0)!
+    }
+
 }
