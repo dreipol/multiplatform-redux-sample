2b22e5ca019e062bfaa971bdbfdca6173981de29
Julia Strasser <julia.strasser@dreipol.ch>
2020-11-02 14:41:00 +0100

    [ios] Do not force cast fonts

--- iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/BrandExtension/UIFont+Brand.swift
+++ iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/BrandExtension/UIFont+Brand.swift
@@ -11,39 +11,46 @@ import UIKit.UILabel
 extension UIFont {
 
     static func h1() -> UIFont {
-        return UIFont(name: "OpenSans-Regular", size: 28.0)!
+        return font(name: "OpenSans-Regular", size: 28.0)
     }
 
     static func h2() -> UIFont {
-        return UIFont(name: "OpenSans-Regular", size: 22.0)!
+        return font(name: "OpenSans-Regular", size: 22.0)
     }
 
     static func h3() -> UIFont {
-        return UIFont(name: "OpenSans-Regular", size: 18.0)!
+        return font(name: "OpenSans-Regular", size: 18.0)
     }
 
     static func h4() -> UIFont {
-        return UIFont(name: "OpenSans-SemiBold", size: 16.0)!
+        return font(name: "OpenSans-SemiBold", size: 16.0)
     }
 
     static func h5() -> UIFont {
-        return UIFont(name: "OpenSans-SemiBold", size: 14.0)!
+        return font(name: "OpenSans-SemiBold", size: 14.0)
     }
 
     static func label() -> UIFont {
-        return UIFont(name: "OpenSans-Bold", size: 14.0)!
+        return font(name: "OpenSans-Bold", size: 14.0)
     }
 
     static func paragraph1() -> UIFont {
-        return UIFont(name: "OpenSans-Regular", size: 16.0)!
+        return font(name: "OpenSans-Regular", size: 16.0)
     }
 
     static func paragraph2() -> UIFont {
-        return UIFont(name: "OpenSans-Regular", size: 14.0)!
+        return font(name: "OpenSans-Regular", size: 14.0)
     }
 
     static func button() -> UIFont {
-        return UIFont(name: "OpenSans-Bold", size: 16.0)!
+        return font(name: "OpenSans-Bold", size: 16.0)
+    }
+
+    private static func font(name: String, size: CGFloat) -> UIFont {
+        guard let font = UIFont(name: name, size: size) else {
+            fatalError("Could not find correct font")
+        }
+        return font
     }
 
 }
