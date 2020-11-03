355f09a8f9c3deb70391bc3feb0dd25d72fc0c16
Kai Widmer <>
2020-11-03 15:26:18 +0100

    [notifications] Reschedule reminder on device reboot

--- app/src/main/java/ch/dreipol/multiplatform/reduxsample/utils/ReminderUtils.kt
+++ app/src/main/java/ch/dreipol/multiplatform/reduxsample/utils/ReminderUtils.kt
@@ -1,6 +1,8 @@
 package ch.dreipol.multiplatform.reduxsample.utils
 
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
 import androidx.work.*
 import ch.dreipol.multiplatform.reduxsample.shared.database.DisposalType
 import ch.dreipol.multiplatform.reduxsample.shared.database.Reminder
@@ -56,5 +58,15 @@ class ReminderWorker(context: Context, workerParams: WorkerParameters) :
         disposalTypes.forEach { showReminderNotification(applicationContext, it) }
         setNextReminderIfPresent(applicationContext)
         return Result.success()
+    }
+}
+
+class BootReceiver : BroadcastReceiver() {
+    @ExperimentalTime
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action != Intent.ACTION_BOOT_COMPLETED) {
+            return
+        }
+        setNextReminderIfPresent(context)
     }
 }
