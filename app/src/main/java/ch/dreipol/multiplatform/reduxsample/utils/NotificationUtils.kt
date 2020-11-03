e5d8641046ef9b3adab65aee5b6dc2009c165336
Kai Widmer <>
2020-11-03 14:09:35 +0100

    [notifications] Add notification utils

---
+++ app/src/main/java/ch/dreipol/multiplatform/reduxsample/utils/NotificationUtils.kt
@@ -0,0 +1,52 @@
+package ch.dreipol.multiplatform.reduxsample.utils
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import ch.dreipol.multiplatform.reduxsample.MainActivity
+import ch.dreipol.multiplatform.reduxsample.R
+import ch.dreipol.multiplatform.reduxsample.shared.database.DisposalType
+
+private const val REMINDER_CHANNEL_ID = "reminder_channel"
+
+fun showReminderNotification(context: Context, disposalType: DisposalType) {
+    val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    createReminderChannel(notificationManager)
+    val title: String = context.resources.getString(R.string.app_name_android)
+    val text = "TODO"
+
+    // Assign BigText style notification - can be opened by the user
+    val bigText = NotificationCompat.BigTextStyle()
+    bigText.setBigContentTitle(title)
+    bigText.bigText(text)
+
+    val mBuilder = NotificationCompat.Builder(context, REMINDER_CHANNEL_ID)
+        .setSmallIcon(context.resources.getIdentifier(disposalType.iconId, "drawable", context.packageName))
+        .setContentTitle(title)
+        .setChannelId(REMINDER_CHANNEL_ID)
+        .setContentText(text)
+        .setAutoCancel(true)
+        .setColor(context.resources.getColor(R.color.test_app_blue, null))
+        .setStyle(bigText)
+
+    val resultIntent = Intent(context, MainActivity::class.java)
+    val resultPendingIntent = PendingIntent.getActivity(
+        context,
+        0,
+        resultIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+    mBuilder.setContentIntent(resultPendingIntent)
+    notificationManager.notify(disposalType.name.hashCode(), mBuilder.build())
+}
+
+private fun createReminderChannel(notificationManager: NotificationManager) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val reminderChannel = NotificationChannel(REMINDER_CHANNEL_ID, "TODO", NotificationManager.IMPORTANCE_HIGH)
+        notificationManager.createNotificationChannel(reminderChannel)
+    }
+}
