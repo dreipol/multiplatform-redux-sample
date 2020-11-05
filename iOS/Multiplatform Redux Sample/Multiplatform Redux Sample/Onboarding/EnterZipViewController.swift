15f58770b54c980ed5b3216bbff713fd267ca563
Julia Strasser <julia.strasser@dreipol.ch>
2020-11-05 08:35:35 +0100

    [ios] Add PLZ label

--- iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Onboarding/EnterZipViewController.swift
+++ iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Onboarding/EnterZipViewController.swift
@@ -11,8 +11,23 @@ import ReduxSampleShared
 
 class EnterZipViewController: BaseOnboardingViewController {
 
+    private let plzLabel = UILabel.label()
+
+    fileprivate func addPLZLabel(_ enterZipState: EnterZipOnboardingState) {
+        plzLabel.text = enterZipState.enterZipViewState.enterZipLabel.localized.uppercased()
+        plzLabel.textColor = UIColor.testAppGreen
+        view.addSubview(plzLabel)
+        plzLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plzLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: kUnit5).isActive = true
+    }
+
     override func render(onboardingSubState: BaseOnboardingSubState) {
         super.render(onboardingSubState: onboardingSubState)
+        guard let enterZipState = onboardingSubState as? EnterZipOnboardingState else {
+            return
+        }
+
+        addPLZLabel(enterZipState)
     }
 
     override func getIndex() -> Int {
