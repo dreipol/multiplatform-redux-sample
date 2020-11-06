73daa33c8896401629db9a912264855c2b566a46
Julia Strasser <julia.strasser@dreipol.ch>
2020-11-06 18:15:37 +0100

    [onboarding_disposal] Add separator for toggle list item view

--- iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Onboarding/ToggleListItem.swift
+++ iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Onboarding/ToggleListItem.swift
@@ -9,8 +9,9 @@ import UIKit
 
 class ToggleListItem: UIControl {
 
-    let imageView: UIImageView = UIImageView.autoLayout()
-    let label = UILabel.h3()
+    private let imageView: UIImageView = UIImageView.autoLayout()
+    private let label = UILabel.h3()
+    private let lineView = UIView.autoLayout()
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -30,22 +31,42 @@ class ToggleListItem: UIControl {
         addSubview(stackView)
         stackView.fillSuperview(edgeInsets: NSDirectionalEdgeInsets(top: kUnit2, leading: kUnit5, bottom: kUnit2, trailing: kUnit5))
 
-        if let imageName = image {
-            imageView.image = UIImage(named: imageName)
-            imageView.setContentHuggingPriority(.required, for: .horizontal)
-            stackView.addArrangedSubview(imageView)
-        }
+        addImage(image, stackView)
+        addLabel(text, stackView)
+        addSwitch(stackView)
+        addLineView()
+    }
 
-        label.text = text?.localized
-        label.textAlignment = .left
-        stackView.addArrangedSubview(label)
 
+    fileprivate func addLineView() {
+        lineView.backgroundColor = UIColor.testAppGreen
+        addSubview(lineView)
+        lineView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        lineView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        lineView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -kUnit5).isActive = true
+        lineView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: kUnit5).isActive = true
+    }
+
+    fileprivate func addSwitch(_ stackView: UIStackView) {
         let toggleSwitch = UISwitch.autoLayout()
         toggleSwitch.isEnabled = true
         toggleSwitch.tintColor = UIColor.testAppGreenDark
         toggleSwitch.setContentHuggingPriority(.required, for: .horizontal)
         stackView.addArrangedSubview(toggleSwitch)
-
     }
 
+    fileprivate func addLabel(_ text: String?, _ stackView: UIStackView) {
+        label.text = text?.localized
+        label.textAlignment = .left
+        label.textColor = UIColor.white
+        stackView.addArrangedSubview(label)
+    }
+
+    fileprivate func addImage(_ image: String?, _ stackView: UIStackView) {
+        if let imageName = image {
+            imageView.image = UIImage(named: imageName)
+            imageView.setContentHuggingPriority(.required, for: .horizontal)
+            stackView.addArrangedSubview(imageView)
+        }
+    }
 }
