7ca5e8910f4c14605bb8f2981eda00efb5318425
Julia Strasser <julia.strasser@dreipol.ch>
2020-11-20 17:39:28 +0100

    [dashboard] Add title of calander view

--- iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Main/CalendarViewController.swift
+++ iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Main/CalendarViewController.swift
@@ -6,12 +6,39 @@
 //
 
 import UIKit
+import ReduxSampleShared
 
-class CalendarViewController: UIViewController {
+class CalendarViewController: PresenterViewController<DashboardView>, DashboardView {
+    override var viewPresenter: Presenter<DashboardView> { DashboardViewKt.dashboardPresenter }
+    private let titleLabel = UILabel.h2()
+    private let scrollView = UIScrollView.autoLayout()
+    private let vStack = UIStackView.autoLayout(axis: .vertical)
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        scrollView.addSubview(vStack)
+        vStack.fitVerticalScrollView()
+        view.addSubview(scrollView)
+        scrollView.fillSuperview(edgeInsets: NSDirectionalEdgeInsets(top: kUnit4, leading: kUnit3, bottom: kUnit3, trailing: kUnit3))
+        vStack.alignment = .fill
+
+        titleLabel.textAlignment = .left
+        vStack.addArrangedSubview(titleLabel)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    func render(viewState_ viewState: DashboardViewState) {
+        titleLabel.text = viewState.titleReplaceable.localized.replacingOccurrences(of: "%s", with: viewState.zip?.stringValue ?? "")
+        print(viewState)
+    }
+
 }
 
 extension CalendarViewController: TabBarCompatible {
