5a4795d098090c115d5e3322276f48735e7d0fe7
Nils Becker <nils.becker@dreipol.ch>
2021-01-20 06:56:07 +0100

    [ios_rating_dialog] Show rating dialog after reminder is shown

--- iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Notifications/NotificationManager.swift
+++ iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Notifications/NotificationManager.swift
@@ -9,18 +9,22 @@ import Combine
 import Foundation
 import ReduxSampleShared
 import UserNotifications
+import StoreKit
 
-class NotificationManager {
+class NotificationManager: NSObject {
     let center = UNUserNotificationCenter.current()
     static let appName: String = (Bundle.main.infoDictionary?[kCFBundleNameKey as String] as? String) ?? ""
     var cancellables = Set<AnyCancellable>()
 
     init(store: Store) {
+        super.init()
+        center.delegate = self
+
         store.settingsStatePublisher().sink { [unowned self] state in
             if let nextReminder = state.nextReminder {
-                schedule(nextReminder)
+                self.schedule(nextReminder)
             } else {
-                center.removeAllPendingNotificationRequests()
+                self.center.removeAllPendingNotificationRequests()
             }
         }.store(in: &cancellables)
 
@@ -47,7 +51,7 @@ class NotificationManager {
     private func schedule(_ reminder: Reminder) {
         cancelAllNotifications()
 
-        var remindDateComponents = reminder.remindDateComponents()
+        let remindDateComponents = reminder.remindDateComponents()
 
         let requests = reminder.disposals.map { disposal -> UNNotificationRequest in
             let content = UNMutableNotificationContent()
@@ -71,7 +75,25 @@ class NotificationManager {
     }
 
     private func cancelAllNotifications() {
-        center.removeAllDeliveredNotifications()
+        center.getDeliveredNotifications(completionHandler: { [center] notifications in
+            if !notifications.isEmpty {
+                SKStoreReviewController.requestReview()
+            }
+
+            center.removeAllDeliveredNotifications()
+        })
         center.removeAllPendingNotificationRequests()
+    }
+}
+
+extension NotificationManager: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        defer {
+            completionHandler()
+        }
+
+        SKStoreReviewController.requestReview()
     }
 }
