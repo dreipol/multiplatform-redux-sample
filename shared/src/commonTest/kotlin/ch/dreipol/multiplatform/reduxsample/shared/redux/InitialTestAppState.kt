b4e2fc5145b6dc26b82fcad44e2638b3cd92b499
Kai Widmer <>
2021-01-21 09:03:59 +0100

    Update tests

--- shared/src/commonTest/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/InitialTestAppState.kt
+++ shared/src/commonTest/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/InitialTestAppState.kt
@@ -1,6 +1,10 @@
 package ch.dreipol.multiplatform.reduxsample.shared.redux
 
+import ch.dreipol.multiplatform.reduxsample.shared.ui.LanguageSettingsViewState
+import ch.dreipol.multiplatform.reduxsample.shared.ui.SettingsViewState
 import ch.dreipol.multiplatform.reduxsample.shared.utils.AppLanguage
 
 val initialTestAppState: AppState
-    get() = AppState(settingsState = SettingsState(appLanguage = AppLanguage.GERMAN))
+    get() = AppState(
+        settingsViewState = SettingsViewState(languageSettingsViewState = LanguageSettingsViewState(appLanguage = AppLanguage.GERMAN))
+    )
--- shared/src/commonTest/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/NavigationReducerTest.kt
+++ shared/src/commonTest/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/NavigationReducerTest.kt
@@ -1,6 +1,7 @@
 package ch.dreipol.multiplatform.reduxsample.shared.redux
 
 import ch.dreipol.multiplatform.reduxsample.shared.redux.actions.NavigationAction
+import ch.dreipol.multiplatform.reduxsample.shared.redux.actions.StateInitializedAction
 import ch.dreipol.multiplatform.reduxsample.shared.redux.reducer.navigationReducer
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -8,25 +9,35 @@ import kotlin.test.assertEquals
 class NavigationReducerTest {
 
     @Test
+    fun initTest() {
+        var navigationState = initialTestAppState.navigationState
+        assertEquals(false, navigationState.initialized)
+
+        navigationState = navigationReducer(navigationState, StateInitializedAction.NAVIGATION)
+        assertEquals(true, navigationState.initialized)
+    }
+
+    @Test
     fun testOnboardingNavigation() {
         var navigationState = initialTestAppState.navigationState
+
         navigationState = navigationReducer(navigationState, NavigationAction.ONBOARDING_START)
-        assertEquals(1, navigationState.screens.size)
-        var lastScreen = navigationState.screens.last() as OnboardingScreen
+        assertEquals(1, navigationState.forceGetState().screens.size)
+        var lastScreen = navigationState.forceGetState().screens.last() as OnboardingScreen
         assertEquals(1, lastScreen.step)
 
         navigationState = navigationReducer(navigationState, NavigationAction.ONBOARDING_NEXT)
-        assertEquals(2, navigationState.screens.size)
-        lastScreen = navigationState.screens.last() as OnboardingScreen
+        assertEquals(2, navigationState.forceGetState().screens.size)
+        lastScreen = navigationState.forceGetState().screens.last() as OnboardingScreen
         assertEquals(2, lastScreen.step)
 
         navigationState = navigationReducer(navigationState, NavigationAction.BACK)
-        assertEquals(1, navigationState.screens.size)
-        lastScreen = navigationState.screens.last() as OnboardingScreen
+        assertEquals(1, navigationState.forceGetState().screens.size)
+        lastScreen = navigationState.forceGetState().screens.last() as OnboardingScreen
         assertEquals(1, lastScreen.step)
 
         navigationState = navigationReducer(navigationState, NavigationAction.ONBOARDING_END)
-        assertEquals(1, navigationState.screens.size)
-        assertEquals(MainScreen.CALENDAR, navigationState.screens.last())
+        assertEquals(1, navigationState.forceGetState().screens.size)
+        assertEquals(MainScreen.CALENDAR, navigationState.forceGetState().screens.last())
     }
 }
