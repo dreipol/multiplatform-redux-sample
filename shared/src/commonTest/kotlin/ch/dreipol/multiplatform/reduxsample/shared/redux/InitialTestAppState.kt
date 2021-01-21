528db3fb1d8a23674c363b731ca2b2944d0e8a5a
Kai Widmer <>
2021-01-21 14:57:09 +0100

    [initilizable_state] Fix tests

--- shared/src/commonTest/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/InitialTestAppState.kt
+++ shared/src/commonTest/kotlin/ch/dreipol/multiplatform/reduxsample/shared/redux/InitialTestAppState.kt
@@ -1,10 +1,13 @@
 package ch.dreipol.multiplatform.reduxsample.shared.redux
 
+import ch.dreipol.dreimultiplatform.reduxkotlin.navigation.NavigationDirection
+import ch.dreipol.dreimultiplatform.reduxkotlin.navigation.NavigationState
 import ch.dreipol.multiplatform.reduxsample.shared.ui.LanguageSettingsViewState
 import ch.dreipol.multiplatform.reduxsample.shared.ui.SettingsViewState
 import ch.dreipol.multiplatform.reduxsample.shared.utils.AppLanguage
 
 val initialTestAppState: AppState
     get() = AppState(
-        settingsViewState = SettingsViewState(languageSettingsViewState = LanguageSettingsViewState(appLanguage = AppLanguage.GERMAN))
+        navigationState = NavigationState(listOf(MainScreen.CALENDAR), NavigationDirection.PUSH),
+        settingsViewState = SettingsViewState(languageSettingsViewState = LanguageSettingsViewState(appLanguage = AppLanguage.GERMAN)),
     )
