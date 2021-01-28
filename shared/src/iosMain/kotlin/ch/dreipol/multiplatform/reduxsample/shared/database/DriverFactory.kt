3b9b540e932d5b48c8b906a3a1f283941cd520ee
Nils Becker <nils.becker@dreipol.ch>
2021-01-28 17:37:39 +0100

    [db_in_shared_container] Use DatabaseConfiguration to specify base path

--- shared/src/iosMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/database/DriverFactory.kt
+++ shared/src/iosMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/database/DriverFactory.kt
@@ -3,9 +3,19 @@ package ch.dreipol.multiplatform.reduxsample.shared.database
 import ch.dreipol.multiplatform.reduxsample.shared.delight.Database
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
+import co.touchlab.sqliter.DatabaseConfiguration
+import platform.Foundation.NSFileManager
+import platform.Foundation.URLByAppendingPathComponent
 
 actual class DriverFactory : DriverCreator {
     actual override fun createDriver(): SqlDriver {
-        return NativeSqliteDriver(Database.Schema, "app.db")
+        val groupUrl = NSFileManager.defaultManager
+            .containerURLForSecurityApplicationGroupIdentifier("group.ch.dreipol.reZHycle")
+        print(groupUrl?.path)
+        val config = DatabaseConfiguration(name = "app.db",
+            basePath = groupUrl!!.path,
+            version = Database.Schema.version,
+            create = { _ -> })
+        return NativeSqliteDriver(config)
     }
 }
