bc75bdf5444da68adae2a7b7ef3310e2b61bfa2d
Julia Strasser <julia.strasser@dreipol.ch>
2021-02-08 08:52:35 +0100

    [fix_safe_areas] Correct bottom margins for iPhone with iOS dock
    Note: First the gradient background view did not go all to the bottom.
    Second it leaded to the jittering on top when scrolling on top.
    https://app.asana.com/0/1199509904376633/1199661788526348/f

--- iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Onboarding/BaseOnboardingViewController.swift
+++ iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Onboarding/BaseOnboardingViewController.swift
@@ -30,7 +30,13 @@ class BaseOnboardingViewController: PresenterViewController<OnboardingSubView>, 
         buttonBackground.backgroundColor = UIColor.clear
         buttonBackground.addSubview(button)
         button.addTarget(self, action: #selector(primayTapped), for: .touchUpInside)
-        let gradientBackgroundHeight: CGFloat = 100.0
+
+        var gradientBackgroundHeight: CGFloat = 100.0
+        if #available(iOS 11, *),
+           let bottomMargin = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first?.safeAreaInsets.bottom,
+           bottomMargin > 0 {
+            gradientBackgroundHeight += bottomMargin
+        }
         let gradient: CAGradientLayer = CAGradientLayer()
         gradient.backgroundColor = UIColor.clear.cgColor
         gradient.isOpaque = false
@@ -49,21 +55,22 @@ class BaseOnboardingViewController: PresenterViewController<OnboardingSubView>, 
 
             buttonBackground.widthAnchor.constraint(equalTo: view.widthAnchor),
             buttonBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            buttonBackground.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            buttonBackground.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             buttonBackground.heightAnchor.constraint(equalToConstant: gradientBackgroundHeight),
 
-            button.bottomAnchor.constraint(equalTo: buttonBackground.bottomAnchor, constant: -kUnit3),
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -kUnit3),
             button.widthAnchor.constraint(equalToConstant: kButtonWidth),
             button.centerXAnchor.constraint(equalTo: buttonBackground.centerXAnchor)
         ])
 
         onboardingScrollView.addSubview(vStack)
         vStack.fitVerticalScrollView()
+        vStack.trailingAnchor.constraint(equalTo: onboardingScrollView.trailingAnchor).isActive = true
         view.addSubview(onboardingScrollView)
         onboardingScrollView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: kUnit2).isActive = true
         onboardingScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         onboardingScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        onboardingScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        onboardingScrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         onboardingScrollView.contentInset = UIEdgeInsets.init(top: 0, left: 0, bottom: gradientBackgroundHeight, right: 0)
         vStack.alignment = .fill
     }
