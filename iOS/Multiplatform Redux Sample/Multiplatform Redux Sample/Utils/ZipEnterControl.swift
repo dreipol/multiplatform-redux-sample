90093bf15d9345aa59725fcce93c509f5697ff58
Nils Becker <nils.becker@dreipol.ch>
2021-02-15 17:51:46 +0100

    [ios-map_and_zip] Indicate invalid zip code
    https://app.asana.com/0/1199509904376633/1199907076496452/f

--- iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Utils/ZipEnterControl.swift
+++ iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Utils/ZipEnterControl.swift
@@ -107,12 +107,20 @@ extension ZipEnterControl: UICollectionViewDataSource {
         // swiftlint:disable force_cast
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kZipCellIdentifier,
                                                       for: indexPath as IndexPath) as! ZipCollectionViewCell
-        cell.label.text = possibleZips[indexPath.row].stringValue
+        if possibleZips.isEmpty {
+            cell.label.text = "zip_invalid".localized
+            cell.label.font = .paragraph2()
+            cell.label.textColor = .monochromesGrey
+        } else {
+            cell.label.text = possibleZips[indexPath.row].stringValue
+            cell.label.font = .h3()
+            cell.label.textColor = .primaryDark
+        }
         return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return possibleZips.count
+        return max(possibleZips.count, 1)
     }
 }
 
