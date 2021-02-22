f733b7831c3c3ba49ee3f760afcdbd975f270a0a
Julia Strasser <julia.strasser@dreipol.ch>
2021-02-22 17:33:21 +0100

    [ios_file_reader] Read json file of iOS
    Note: But they are not visible in the viewState afterwards.

--- shared/src/iosMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/utils/FileReader.kt
+++ shared/src/iosMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/utils/FileReader.kt
@@ -1,7 +1,16 @@
 package ch.dreipol.multiplatform.reduxsample.shared.utils
 
+import ch.dreipol.dreimultiplatform.kermit
+import platform.Foundation.NSBundle
+import platform.Foundation.NSString
+import platform.Foundation.NSUTF8StringEncoding
+import platform.Foundation.stringWithContentsOfFile
+
 actual class FileReader {
     actual fun readCollectionPointsFile(): String {
-        throw NotImplementedError("TODO")
+        val filePath = NSBundle.mainBundle().pathForResource("poi_sammelstelle_view", "json")
+        val string = NSString.stringWithContentsOfFile(filePath.toString(), NSUTF8StringEncoding, null) ?: return ""
+        kermit().d({ "string of json: $string" })
+        return string
     }
 }
