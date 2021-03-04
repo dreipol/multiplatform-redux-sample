ab782db08516480b50a5b212d5379d18ac5510de
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2021-03-04 14:45:43 +0100

    [swisstopo] Add swisstopo map and show zurich

--- iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Main/CollectionPointMapViewController.swift
+++ iOS/Multiplatform Redux Sample/Multiplatform Redux Sample/Main/CollectionPointMapViewController.swift
@@ -5,9 +5,11 @@
 //  Created by Samuel Bichsel on 30.10.20.
 //
 
-import UIKit
-import ReduxSampleShared
+import MapCoreSharedModule
 import MapKit
+import ReduxSampleShared
+import SwisstopoMapSDK
+import UIKit
 
 private let zurichLat = 47.3744489
 private let zurichLon = 8.5410422
@@ -15,44 +17,37 @@ private let zurichLon = 8.5410422
 class CollectionPointMapViewController: BasePresenterViewController<CollectionPointMapView>, CollectionPointMapView {
     override var viewPresenter: Presenter<CollectionPointMapView> { CollectionPointMapViewKt.collectionPointMapPresenter }
     private let titleLabel = UILabel.h2()
-    private let mapView = MKMapView()
+    private var mapView = SwisstopoMapView()
 
     override init() {
         super.init()
-
+        mapView.camera.addListener(self)
         mapView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(mapView)
         mapView.fitSuperview()
-        let zurich = CLLocationCoordinate2D(latitude: zurichLat, longitude: zurichLon)
-        mapView.setRegion(MKCoordinateRegion(center: zurich, latitudinalMeters: 10_000, longitudinalMeters: 10_000), animated: true)
-
-        let container = UIView.autoLayout()
-        container.backgroundColor = .white
-        container.layer.cornerRadius = 5
-        container.layer.masksToBounds = true
-
-        titleLabel.text = "map_work_in_progress".localized
-        container.addSubview(titleLabel)
-        titleLabel.fillSuperviewMargins()
-
-        view.addSubview(container)
-        NSLayoutConstraint.activate([
-            container.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            container.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            container.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -2 * kUnit2),
-        ])
+        mapView.camera.move(toCenterPosition: .init(systemIdentifier: MCCoordinateSystemIdentifiers.epsg4326(),
+                                                    x: zurichLon,
+                                                    y: zurichLat,
+                                                    z: 0), animated: false)
+        mapView.camera.setZoom(174901, animated: false)
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func render(collectionPointMapViewState: CollectionPointMapViewState) {
-
-    }
-
+    func render(collectionPointMapViewState: CollectionPointMapViewState) {}
 }
 
 extension CollectionPointMapViewController: TabBarCompatible {
     var tabBarImageName: String { "ic_32_location" }
+}
+
+extension CollectionPointMapViewController: MCMapCamera2dListenerInterface {
+    func onVisibleBoundsChanged(_ visibleBounds: MCRectCoord, zoom: Double) {
+        print("bounds: \(visibleBounds)\n zoom: \(zoom)")
+    }
+
+    func onRotationChanged(_ angle: Float) {}
 }
