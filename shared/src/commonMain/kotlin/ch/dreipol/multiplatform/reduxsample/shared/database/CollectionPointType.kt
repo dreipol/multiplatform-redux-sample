873ef86cd44fa4235966710a60517aa1ca4894eb
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2021-03-08 14:44:34 +0100

    [swisstopo] Fix collectionpointtype icon in Swift

--- shared/src/commonMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/database/CollectionPointType.kt
+++ shared/src/commonMain/kotlin/ch/dreipol/multiplatform/reduxsample/shared/database/CollectionPointType.kt
@@ -1,6 +1,6 @@
 package ch.dreipol.multiplatform.reduxsample.shared.database
 
-enum class CollectionPointType(val translationKey: String, icon: String) {
+enum class CollectionPointType(val translationKey: String, val icon: String) {
     GLASS("collection_point_type_glass", "ic_24_glas_colored"),
     METAL("collection_point_type_metal", "ic_24_tin_colored"),
     OIL("collection_point_type_oil", "ic_24_oil_colored"),
