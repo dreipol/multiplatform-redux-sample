a804af8958544df314966282a7bb5c7e5cc5e8bd
Nils Becker <nils.becker@dreipol.ch>
2021-04-09 14:11:23 +0200

    [map_location] Translate location usage description

--- iOS/Multiplatform Redux Sample/Scripts/download_translations.sh
+++ iOS/Multiplatform Redux Sample/Scripts/download_translations.sh
@@ -6,8 +6,10 @@ DIR="$(cd "$(dirname "$0")"; pwd)";
 function downloadLanguage {
     echo "fetching locale $1"
     TARGET_FILE="$SRCROOT/$PROJECT_NAME/Resources/$2.lproj/Localizable.strings"
+    INFO_PLIST_FILE="$SRCROOT/$PROJECT_NAME/Resources/$2.lproj/InfoPlist.strings"
     rm $TARGET_FILE 2> /dev/null
-    curl -f -L -s "https://localise.biz:443/api/export/locale/$1.strings?key=$READONLY_KEY" | sed -e '6,7d' > "${TARGET_FILE}"
+    curl -f -L -s "https://localise.biz:443/api/export/locale/$1.strings?filter=!infoPlist&key=$READONLY_KEY" | sed -e '6,7d' > "${TARGET_FILE}"
+    curl -f -L -s "https://localise.biz:443/api/export/locale/$1.strings?filter=infoPlist&key=$READONLY_KEY" | sed -e '6,7d' > "${INFO_PLIST_FILE}"
 }
 
 languages=( "de" "en" )
