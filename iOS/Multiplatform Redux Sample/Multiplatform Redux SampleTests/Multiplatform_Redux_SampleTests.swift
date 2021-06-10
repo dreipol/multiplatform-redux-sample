2d50c4d313b68d297c3537d9f3c1a6760c99b5a7
Nils Becker <nils.becker@dreipol.ch>
2021-06-10 13:40:59 +0200

    [update_license] Add copyright to header comments in iOS

--- iOS/Multiplatform Redux Sample/Multiplatform Redux SampleTests/Multiplatform_Redux_SampleTests.swift
+++ iOS/Multiplatform Redux Sample/Multiplatform Redux SampleTests/Multiplatform_Redux_SampleTests.swift
@@ -1,6 +1,11 @@
 //
 //  Multiplatform_Redux_SampleTests.swift
-//  Multiplatform Redux SampleTests
+//  Multiplatform Redux Sample
+//
+//  Copyright (c) 2021 dreipol GmbH
+//
+//  Licensed under a Creative Commons Attribution-NonCommercial 4.0 International License. A copy of the license is provided with this code.
+//  No warranties are given. The license may not give you all of the permissions necessary for your intended use. For example, other rights such as publicity, privacy, or moral rights may limit how you use the material.Tests
 //
 //  Created by Samuel Bichsel on 22.10.20.
 //
--- iOS/Multiplatform Redux Sample/Multiplatform Redux SampleUITests/Multiplatform_Redux_SampleUITests.swift
+++ iOS/Multiplatform Redux Sample/Multiplatform Redux SampleUITests/Multiplatform_Redux_SampleUITests.swift
@@ -1,6 +1,11 @@
 //
 //  Multiplatform_Redux_SampleUITests.swift
-//  Multiplatform Redux SampleUITests
+//  Multiplatform Redux Sample
+//
+//  Copyright (c) 2021 dreipol GmbH
+//
+//  Licensed under a Creative Commons Attribution-NonCommercial 4.0 International License. A copy of the license is provided with this code.
+//  No warranties are given. The license may not give you all of the permissions necessary for your intended use. For example, other rights such as publicity, privacy, or moral rights may limit how you use the material.UITests
 //
 //  Created by Samuel Bichsel on 22.10.20.
 //
